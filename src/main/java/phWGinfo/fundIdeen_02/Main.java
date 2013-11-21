86c05c12e208c3798272d0c2a51da57c124529a7
Paul Libbrecht <paul@hoplahup.net>
2013-11-21 11:58:17 +0100

    Bereit für Vorlesung 5. paul

--- src/main/java/phWGinfo/fundIdeen_02/Main.java
+++ src/main/java/phWGinfo/fundIdeen_02/Main.java
@@ -10,6 +10,25 @@ public class Main {
         // Methoden Parametern
         // new DemoMethodenParameter().run();
 
+        Lebewesen m = new Mensch();
+        m.sagWas("was");
+
+        Lebewesen l = new Lebewesen();
+        l.sagWas("was");
     }
 
+}
+
+class Lebewesen {
+    void sagWas(String s) {
+        System.out.println(s);
+    }
+}
+
+class Mensch extends Lebewesen{
+   // Glied bein, fuss;
+    void sagWas(String s) {
+        System.out.println(
+                "Mund: " + s);
+    }
 }
