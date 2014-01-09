b20446f68dbd847ed83abca2c877343e78125e10
Paul Libbrecht <paul@hoplahup.net>
2014-01-09 12:20:11 +0100

    mit Kommentar

--- src/main/java/phWGinfo/fundIdeen_02/Main.java
+++ src/main/java/phWGinfo/fundIdeen_02/Main.java
@@ -9,6 +9,7 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) throws Throwable {
+        // erzeugt ein Objekt der Klasse "Sache" mit bestimmte Werte
         Sache s = new Sache("feldWert", 3);
     }
 
