a022616e69171fc0de8b8d49af49b30b41a5c65a
Paul Libbrecht <paul@hoplahup.net>
2014-01-09 12:08:57 +0100

    nix

--- src/main/java/phWGinfo/fundIdeen_02/Main.java
+++ src/main/java/phWGinfo/fundIdeen_02/Main.java
@@ -1,36 +1,15 @@
 package phWGinfo.fundIdeen_02;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  */
 public class Main {
 
     public static void main(String[] args) throws Throwable {
-
-        // Methoden Parametern
-        // new DemoMethodenParameter().run();
-
-
-        String x=null;
-        if(x==null) {
-            System.out.println("x ist null");
-        } else {
-            System.out.println("x ist nicht null");
-        }
+        Sache s = new Sache();
     }
 
-}
-
-class Lebewesen {
-    void sagWas(String s) {
-        System.out.println(s);
-    }
-}
-
-class Mensch extends Lebewesen{
-   // Glied bein, fuss;
-    void sagWas(String s) {
-        System.out.println(
-                "Mund: " + s);
-    }
 }
