6668556462c6b555eddd16a181a30e224073f015
Paul Libbrecht <paul@hoplahup.net>
2014-01-09 11:55:12 +0100

    DIng nach Sache.

---
+++ src/main/java/phWGinfo/fundIdeen_02/Sache.java
@@ -0,0 +1,24 @@
+package phWGinfo.fundIdeen_02;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: paul
+ * Date: 14/11/13
+ * Time: 12:01
+ * To change this template use File | Settings | File Templates.
+ */
+class Sache {
+
+    public Sache() {
+        System.out.println("Konstruktor wurde gerufen.");
+    }
+
+    String feld1 = "";
+    int x = 0;
+
+    void reset() {
+        feld1 = "";
+        x = 0;
+    }
+
+}
